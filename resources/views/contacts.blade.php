4bd885fc766933f6474775928f4cc95eb295c269
Muhammad Iqmal <iqmalazim@gmail.com>
2017-05-24 13:08:32 +0800

    asd

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -97,7 +97,7 @@
 
 
                 <li>
-                    <a href="login.html">
+                    <a href="/logout">
                         <i class="fa fa-sign-out"></i> Log out
                     </a>
                 </li>
